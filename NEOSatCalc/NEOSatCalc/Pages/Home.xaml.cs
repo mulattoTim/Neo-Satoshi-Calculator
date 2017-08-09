b391c9d2f65330e0e691e090ac79ef419284c695
mulattoTim <tim.franklin4603@gmail.com>
2017-08-09 08:10:50 -0400

    pulled in httpclient and bittrex api wrapper

---
+++ NEOSatCalc/NEOSatCalc/Pages/Home.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Net;
+using Bittrex;
+
+namespace NEOSatCalc.Pages
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : UserControl
+    {
+        public Home()
+        {
+            InitializeComponent();
+            GetBitcoinPrice();
+
+        }
+
+        public async Task<JsonObject> GetBitcoinPRice()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(uri);
+
+            //will throw an exception if not successful
+            response.EnsureSuccessStatusCode();
+
+            string content = await response.Content.ReadAsStringAsync();
+            return await (Task.Run() = > JsonObject.Parse(content));
+        }
+
+    }
+    }
+}
